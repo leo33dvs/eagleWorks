de726b6905d43b48b2af0727e439bc0a81ad69e8
LEONARDO DE LIMA EMIDIO <emidio.leonardo33@gmail.com>
2025-04-27 02:28:08 -0300

    Adiciona logout.php

---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'includes/functions.php';
+
+// Start session if not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to homepage
+redirect('index.php');
+?>
